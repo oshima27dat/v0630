a7c4fb8540eb2d65a8ecb58883bcbe53aa4416e3
A601-03 <oshima.dat22@gmail.com>
2022-06-30 16:06:49 +0900

    配列

--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,13 +12,19 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx = rand.Next(-10, 11),vx1 = rand.Next(-10,11),vx2 = rand.Next(-10,11)
-          , vy = rand.Next(-10,11),vy1 = rand.Next(-10,11),vy2 = rand.Next(-10,11);
+        int []vx = new int[3];
+        int []vy = new int[3];
         //静的=最初に決めておく <> 動的=実行時に変更可能
         static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
+            vx[0] = rand.Next(-10, 11);
+            vx[1] = rand.Next(-10, 11);
+            vx[2] = rand.Next(-10, 11);
+            vy[0] = rand.Next(-10, 11);
+            vy[1] = rand.Next(-10, 11);
+            vy[2] = rand.Next(-10, 11);
             label1.Left = rand.Next(0,ClientSize.Width-label1.Width);
             label1.Top = rand.Next(0,ClientSize.Height-label1.Height);
             label3.Left = rand.Next(0, ClientSize.Width - label1.Width);
@@ -33,28 +39,28 @@ namespace v0630
             Point fpos = PointToClient(spos);
             label2.Left = fpos.X-label2.Width/2;
             label2.Top = fpos.Y-label2.Height/2;
-            label1.Left += vx;
-            label1.Top += vy;
-            label3.Left += vx1;
-            label3.Top += vy1;
-            label4.Left += vx2;
-            label4.Top += vy2;
+            label1.Left += vx[0];
+            label1.Top += vy[0];
+            label3.Left += vx[1];
+            label3.Top += vy[1];
+            label4.Left += vx[2];
+            label4.Top += vy[2];
 
             if (label1.Left < 0)
             {
-                vx = Math.Abs(vx);
+                vx[0] = Math.Abs(vx[0]);
             }
             if (label1.Right > ClientSize.Width)
             {
-                vx = -Math.Abs(vx);
+                vx[0] = -Math.Abs(vx[0]);
             }
             if (label1.Top < 0)
             {
-                vy = Math.Abs(vy);
+                vy[0] = Math.Abs(vy[0]);
             }
             if (label1.Bottom > ClientSize.Height)
             {
-                vy = -Math.Abs(vy);
+                vy[0] = -Math.Abs(vy[0]);
             }
             if((label1.Left <= fpos.X)&&(label1.Right>fpos.X)&&(label1.Top<=fpos.Y)&&(label1.Bottom>fpos.Y))
             {
@@ -63,19 +69,19 @@ namespace v0630
 
             if (label3.Left < 0)
             {
-                vx1 = Math.Abs(vx);
+                vx[1] = Math.Abs(vx[1]);
             }
             if (label3.Right > ClientSize.Width)
             {
-                vx1 = -Math.Abs(vx);
+                vx[1] = -Math.Abs(vx[1]);
             }
             if (label3.Top < 0)
             {
-                vy1 = Math.Abs(vy);
+                vy[1] = Math.Abs(vy[1]);
             }
             if (label3.Bottom > ClientSize.Height)
             {
-                vy1 = -Math.Abs(vy);
+                vy[1] = -Math.Abs(vy[1]);
             }
             if ((label3.Left <= fpos.X) && (label3.Right > fpos.X) && (label3.Top <= fpos.Y) && (label3.Bottom > fpos.Y))
             {
@@ -84,19 +90,19 @@ namespace v0630
 
             if (label4.Left < 0)
             {
-                vx2 = Math.Abs(vx);
+                vx[2] = Math.Abs(vx[2]);
             }
             if (label4.Right > ClientSize.Width)
             {
-                vx2 = -Math.Abs(vx);
+                vx[2] = -Math.Abs(vx[2]);
             }
             if (label4.Top < 0)
             {
-                vy2 = Math.Abs(vy);
+                vy[2] = Math.Abs(vy[2]);
             }
             if (label4.Bottom > ClientSize.Height)
             {
-                vy2 = -Math.Abs(vy);
+                vy[2] = -Math.Abs(vy[2]);
             }
             if ((label4.Left <= fpos.X) && (label4.Right > fpos.X) && (label4.Top <= fpos.Y) && (label4.Bottom > fpos.Y))
             {

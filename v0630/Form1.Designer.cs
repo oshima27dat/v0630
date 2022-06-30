c56db39313595079f9194bcfaf3f323ec38d7332
A601-03 <oshima.dat22@gmail.com>
2022-06-30 15:59:06 +0900

    3つ動作

--- v0630/Form1.Designer.cs
+++ v0630/Form1.Designer.cs
@@ -33,13 +33,15 @@ namespace v0630
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(310, 226);
+            this.label1.Location = new System.Drawing.Point(25, 39);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 21);
             this.label1.TabIndex = 0;
@@ -49,7 +51,7 @@ namespace v0630
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(499, 226);
+            this.label2.Location = new System.Drawing.Point(353, 453);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(49, 21);
             this.label2.TabIndex = 1;
@@ -61,11 +63,33 @@ namespace v0630
             this.timer1.Enabled = true;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(379, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 21);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "★";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(272, 135);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 21);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "★";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
@@ -80,6 +104,8 @@ namespace v0630
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- v0630/Form1.cs
+++ v0630/Form1.cs
@@ -12,14 +12,19 @@ namespace v0630
 {
     public partial class Form1 : Form
     {
-        int vx = rand.Next(-10,11), vy = rand.Next(-10,11);
+        int vx = rand.Next(-10, 11),vx1 = rand.Next(-10,11),vx2 = rand.Next(-10,11)
+          , vy = rand.Next(-10,11),vy1 = rand.Next(-10,11),vy2 = rand.Next(-10,11);
         //静的=最初に決めておく <> 動的=実行時に変更可能
         static Random rand = new Random();
         public Form1()
         {
             InitializeComponent();
-            label1.Left = rand.Next(0,ClientSize.Width);
-            label1.Top = rand.Next(0,ClientSize.Height);
+            label1.Left = rand.Next(0,ClientSize.Width-label1.Width);
+            label1.Top = rand.Next(0,ClientSize.Height-label1.Height);
+            label3.Left = rand.Next(0, ClientSize.Width - label1.Width);
+            label3.Top = rand.Next(0, ClientSize.Height - label1.Height);
+            label4.Left = rand.Next(0, ClientSize.Width - label1.Width);
+            label4.Top = rand.Next(0, ClientSize.Height - label1.Height);
         }
 
         private void timer1_Tick(object sender, EventArgs e)
@@ -30,6 +35,10 @@ namespace v0630
             label2.Top = fpos.Y-label2.Height/2;
             label1.Left += vx;
             label1.Top += vy;
+            label3.Left += vx1;
+            label3.Top += vy1;
+            label4.Left += vx2;
+            label4.Top += vy2;
 
             if (label1.Left < 0)
             {
@@ -51,8 +60,50 @@ namespace v0630
             {
                 timer1.Enabled = false;
             }
-        }
 
+            if (label3.Left < 0)
+            {
+                vx1 = Math.Abs(vx);
+            }
+            if (label3.Right > ClientSize.Width)
+            {
+                vx1 = -Math.Abs(vx);
+            }
+            if (label3.Top < 0)
+            {
+                vy1 = Math.Abs(vy);
+            }
+            if (label3.Bottom > ClientSize.Height)
+            {
+                vy1 = -Math.Abs(vy);
+            }
+            if ((label3.Left <= fpos.X) && (label3.Right > fpos.X) && (label3.Top <= fpos.Y) && (label3.Bottom > fpos.Y))
+            {
+                timer1.Enabled = false;
+            }
+
+            if (label4.Left < 0)
+            {
+                vx2 = Math.Abs(vx);
+            }
+            if (label4.Right > ClientSize.Width)
+            {
+                vx2 = -Math.Abs(vx);
+            }
+            if (label4.Top < 0)
+            {
+                vy2 = Math.Abs(vy);
+            }
+            if (label4.Bottom > ClientSize.Height)
+            {
+                vy2 = -Math.Abs(vy);
+            }
+            if ((label4.Left <= fpos.X) && (label4.Right > fpos.X) && (label4.Top <= fpos.Y) && (label4.Bottom > fpos.Y))
+            {
+                timer1.Enabled = false;
+            }
+        }
+        
         private void label2_Click(object sender, EventArgs e)
         {
 
